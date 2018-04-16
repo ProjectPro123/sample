3a407712e6f6f41fdcac56c903d6221335df4313
ravi <rkthorizon@gmail.com>
2018-04-16 12:28:35 +0530

    Committing the fiels

---
+++ src/main/java/sample/App.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class App {
+	
+public String sample() {
+	return "sample Program";
+}
+
+}
---
+++ src/test/java/sample/AppTest.java
@@ -0,0 +1,13 @@
+package sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	}
+
+}
